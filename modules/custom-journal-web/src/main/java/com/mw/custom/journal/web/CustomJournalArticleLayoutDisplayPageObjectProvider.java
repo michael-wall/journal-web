64428236a7aa5765f85a36316b7ed2d359765d94
Michael Wall <Michael Wall@host.docker.internal>
2024-08-22 14:57:30 +0100

    Initial checkin of POC

---
+++ modules/custom-journal-web/src/main/java/com/mw/custom/journal/web/CustomJournalArticleLayoutDisplayPageObjectProvider.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.mw.custom.journal.web;
+
+import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.model.AssetRenderer;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.asset.util.AssetHelper;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.layout.display.page.LayoutDisplayPageObjectProvider;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.Locale;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class CustomJournalArticleLayoutDisplayPageObjectProvider
+	implements LayoutDisplayPageObjectProvider<JournalArticle> {
+
+	public CustomJournalArticleLayoutDisplayPageObjectProvider(
+			JournalArticle article, AssetHelper assetHelper)
+		throws PortalException {
+
+		_article = article;
+		_assetHelper = assetHelper;
+
+		_assetEntry = _getAssetEntry(article);
+	}
+
+	@Override
+	public String getClassName() {
+		return JournalArticle.class.getName();
+	}
+
+	@Override
+	public long getClassNameId() {
+		return _assetEntry.getClassNameId();
+	}
+
+	@Override
+	public long getClassPK() {
+		return _article.getResourcePrimKey();
+	}
+
+	@Override
+	public long getClassTypeId() {
+		return _assetEntry.getClassTypeId();
+	}
+
+	@Override
+	public String getDescription(Locale locale) {
+		return _assetEntry.getDescription(locale);
+	}
+
+	@Override
+	public JournalArticle getDisplayObject() {
+		return _article;
+	}
+
+	@Override
+	public long getGroupId() {
+		return _article.getGroupId();
+	}
+
+	@Override
+	public String getKeywords(Locale locale) {
+		return _assetHelper.getAssetKeywords(
+			_assetEntry.getClassName(), _assetEntry.getClassPK(), locale);
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return _assetEntry.getTitle(locale);
+	}
+
+	@Override
+	public String getURLTitle(Locale locale) {
+		AssetRenderer<?> assetRenderer = _assetEntry.getAssetRenderer();
+
+		return assetRenderer.getUrlTitle(locale);
+	}
+
+	private AssetEntry _getAssetEntry(JournalArticle journalArticle)
+		throws PortalException {
+
+		AssetRendererFactory<?> assetRendererFactory =
+			AssetRendererFactoryRegistryUtil.getAssetRendererFactoryByClassName(
+				JournalArticle.class.getName());
+
+		return assetRendererFactory.getAssetEntry(
+			JournalArticle.class.getName(),
+			journalArticle.getResourcePrimKey());
+	}
+
+	private final JournalArticle _article;
+	private final AssetEntry _assetEntry;
+	private final AssetHelper _assetHelper;
+
+}
